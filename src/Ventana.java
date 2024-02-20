d2f8ebdb1a4baaeb47b64aa3b2ddb83f64c57262
Jonathan soto <jsoto1429@gmail.com>
2024-02-20 08:50:45 -0700

    add JPanel to JFrame

---
+++ src/Ventana.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Ventana extends JFrame{
+	
+	//contructor que define los atributos base
+	//de mi ventana
+	public Ventana(){
+		
+		this.setVisible(true);
+		this.setSize(200, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Mi ventana");
+		this.setMinimumSize(new Dimension(250,250));
+		this.setMaximumSize(new Dimension(750,750));
+		this.setResizable(true);
+		this.setLocation(200, 200);
+		this.setLocationRelativeTo(null);
+		
+		this.iniciarComponentes();
+	}
+	
+	public void iniciarComponentes() {
+		
+		JPanel login = new JPanel();
+		login.setSize(this.getWidth(), this.getHeight());
+		login.setBackground(Color.red);
+		
+		this.add(login);
+		
+	}
+}
