75ab25bcb308a23e29f9825812c15d2f52464855
Jonathan soto <jsoto1429@gmail.com>
2024-02-22 07:20:49 -0700

    diseño final de registro, función registro

--- src/Ventana.java
+++ src/Ventana.java
@@ -2,12 +2,17 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
@@ -17,7 +22,7 @@ public class Ventana extends JFrame{
 	public Ventana(){
 		
 		this.setVisible(true);
-		this.setSize(1000, 500);
+		this.setSize(1000, 750);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Mi ventana");
 		this.setMinimumSize(new Dimension(250,250));
@@ -32,6 +37,16 @@ public class Ventana extends JFrame{
 	}
 	
 	public void iniciarComponentes() {
+		
+		this.login();
+		
+		this.registro();
+		
+		this.repaint();
+		this.validate();
+	}
+	
+	public void login() {
 		
 		JPanel login = new JPanel();
 		login.setSize(this.getWidth()/2, this.getHeight());
@@ -83,15 +98,110 @@ public class Ventana extends JFrame{
 		//size
 		//location
 		//background - opaque 
-		//string constructor
+		//string constructor 
+		
+		this.add(login);
+		
+		
+	}
+	
+	
+	public void registro() {
 		
 		JPanel registro = new JPanel();
 		registro.setSize(this.getWidth()/2, this.getHeight());
 		registro.setLocation(500, 0);
-		registro.setBackground(Color.green);
+		registro.setBackground(Color.decode("#47CBEC"));
+		registro.setLayout(null);
 		
-		this.add(login);
-		//this.add(registro);
+		JLabel regis_tag = new JLabel("REGISTRO",0);
+		regis_tag.setSize(300, 80);
+		regis_tag.setFont( new Font("Marker Felt", Font.BOLD, 40) );
+		regis_tag.setForeground(Color.white);
+		regis_tag.setLocation(100, 20);
+		regis_tag.setOpaque(true);
+		regis_tag.setBackground(Color.black);
+		registro.add(regis_tag);
+		
+		JLabel usr_tag = new JLabel("NOMBRE DE USUARIO: ",0);
+		usr_tag.setBounds(50, 120, 400, 40);
+		usr_tag.setFont( new Font("Marker Felt", Font.BOLD, 20) );
+		usr_tag.setOpaque(true);
+		usr_tag.setBackground(Color.yellow);
+		registro.add(usr_tag);
+		
+		JTextField usr_field = new JTextField();
+		usr_field.setBounds(50, 160, 400, 40);
+		usr_field.setHorizontalAlignment(0);
+		usr_field.setFont(new Font("Marker Felt", Font.PLAIN, 17));
+		registro.add(usr_field);
+		
+		JLabel bio_tag = new JLabel("BIO",0);
+		bio_tag.setBounds(50, 200, 400, 40);
+		bio_tag.setFont( new Font("Marker Felt", Font.BOLD, 20) );
+		registro.add(bio_tag);
+		
+		JTextArea bio_tex = new JTextArea();
+		bio_tex.setBounds(50, 240, 400, 80);
+		bio_tex.setFont( new Font("Marker Felt", Font.BOLD, 20) );
+		registro.add(bio_tex);
+		
+		JLabel prefer_tag = new JLabel("PREFERENCIAS",0);
+		prefer_tag.setBounds(50, 320, 400, 40);
+		prefer_tag.setFont( new Font("Marker Felt", Font.BOLD, 20) );
+		registro.add(prefer_tag);
+		
+		JCheckBox item_box_1 = new JCheckBox("Dulces");
+		item_box_1.setBounds(50, 360, 80, 40);
+		item_box_1.setFont(new Font("Marker Felt", Font.BOLD, 20)); 
+		registro.add(item_box_1);
+		
+		JCheckBox item_box_2 = new JCheckBox("Salado");
+		item_box_2.setBounds(140, 360, 90, 40);
+		item_box_2.setFont(new Font("Marker Felt", Font.BOLD, 20));
+		registro.add(item_box_2);
+		
+		JCheckBox item_box_3 = new JCheckBox("Saludable");
+		item_box_3.setBounds(240, 360, 110, 40);
+		item_box_3.setFont(new Font("Marker Felt", Font.BOLD, 20));
+		item_box_3.setBorderPainted(true);
+		item_box_3.setBorder(BorderFactory.createLineBorder(Color.red,2));
+		registro.add(item_box_3);
+		
+		JLabel terms_tag = new JLabel("TÉRMINOS",0);
+		terms_tag.setBounds(50, 400, 400, 40);
+		terms_tag.setFont( new Font("Marker Felt", Font.BOLD, 20) );
+		terms_tag.setOpaque(true);
+		terms_tag.setBackground(Color.yellow);
+		registro.add(terms_tag);
+		
+		JRadioButton accept_radio = new JRadioButton("Acepto los términos");
+		accept_radio.setBounds(50, 440, 200, 40);
+		accept_radio.setFont( new Font("Marker Felt", Font.BOLD, 20) );
+		registro.add(accept_radio);
+		
+		JRadioButton reject_radio = new JRadioButton("No acepto los términos");
+		reject_radio.setBounds(250, 440, 400, 40);
+		reject_radio.setFont( new Font("Marker Felt", Font.BOLD, 20) );
+		registro.add(reject_radio);
+		
+		ButtonGroup terms = new ButtonGroup();
+		terms.add(accept_radio);
+		terms.add(reject_radio);
+		
+		String colinias[]={"Centro","Camino Real","Pedregal","Conchalito","La 8"}; 
+		
+		JComboBox locations = new JComboBox(colinias);
+		locations.setBounds(50, 480, 400, 40);
+		locations.setFont(new Font("Marker Felt", Font.BOLD, 20));
+		registro.add(locations);
+		
+		JButton rgs_btn = new JButton("Crear cuenta");
+		rgs_btn.setBounds(50, 530, 400, 70);
+		rgs_btn.setFont( new Font("Marker Felt", Font.BOLD, 30) );
+		registro.add(rgs_btn);
+		
+		this.add(registro); 
 		
 	}
 }
